ef0db1faa9a5f49c69d9223055bcef2bd69efe65
Marc Poppy <poppy.marc@gmail.com>
2019-06-04 05:00:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    #need to evaluate self. split on any punctuation and then count_sentences
+    sentences = self.split(/[.?!]/).reject(&:empty?)
 
+    sentences.count
   end
 end
